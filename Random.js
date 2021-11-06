16004c4706c097bddf3335f7f4ecbcbc2593d9f8
MADAVARAM LAYA <madavaramlaya@gmail.com>
2021-11-06 11:24:19 +0530

    Add files via upload

---
+++ Random.js
@@ -0,0 +1,27 @@
+function random_num(){
+    var N=parseInt((document.getElementById("num")).value);
+    var n=Math.floor(Math.random()*N)+1;
+    var chances=Math.floor(Math.sqrt(N));
+    var count=0;
+    while(true){
+        ++count;
+        if(count<=chances){
+            guess=parseInt(prompt("Enter the number you guessed : "));
+            if(guess<n){
+                alert("Too low. Try again...\nChances remaining : "+(chances-count));
+            }
+            else if(guess>n){
+                alert("Too high. Try again...\nChances remaining : "+(chances-count));
+            }
+            else{
+                document.write('<h1 align="center" style="color: blue;padding-top: 5%;">Congratulations...! You won.</h1>');
+                break;
+            }
+        }
+        else{
+            document.write('<h1 align="center" style="color: blue;padding-top: 5%;">Oops...! You lose the game.</h1><br>');
+            document.write('<h2 align="center" style="color: orange;">The correct number is '+n+'.</h2>');
+            break;
+        }
+    }
+}
